d2c760f0e73577e0cad7d0f052d5a7343b095be1
Matvei <105492881+matb3l@users.noreply.github.com>
2024-12-11 17:09:14 +0300

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,5 +6,5 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: 6543
+    port: 5432,
 });
